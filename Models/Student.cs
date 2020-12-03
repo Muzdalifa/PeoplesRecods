6458b1bfe50cd3a0c0513160d49dc295d314e262
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2020-12-03 11:46:19 +0100

    Giving proper error message on form validation

--- Models/Student.cs
+++ Models/Student.cs
@@ -12,17 +12,17 @@ namespace RaorPages.Models
     {
         public int Id { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Please fill the first name")]
         [RegularExpression(@"[A-z]*", ErrorMessage = "Use only alphabets.")]
-        [StringLength(50, ErrorMessage = "First name cannot be empty or longer than 50 characters.")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstName { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Please fill the last name")]
         [RegularExpression(@"[A-z]*", ErrorMessage = "Use only alphabets.")]
-        [StringLength(50,ErrorMessage = "Last name cannot be empty or longer than 50 characters.")]
+        [StringLength(50,ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please fill the phone number")]
         [RegularExpression(@"[+][0-9]*$", ErrorMessage = "Please start with + ")] //PhoneNumber has start with +
         public string PhoneNumber { get; set; }
         
